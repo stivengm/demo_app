a6ca9e19526580baee1e543a5966e9bbc6548da0
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 18:30:48 -0500

    ADD - Componente videoChatView.

---
+++ lib/ui/views/video_chat_view/video_chat_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class VideoChatView extends StatelessWidget {
+  const VideoChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
