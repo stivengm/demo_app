53db5244426f8cc515d435b9a0ab0f5989dfa249
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 18:39:19 -0500

    ADD - Componente login.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,3 +1,4 @@
+import 'package:demo_app/ui/views/user/login_view/login_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:demo_app/ui/views/home_view/home_view.dart';
@@ -6,5 +7,6 @@ import 'package:demo_app/ui/views/video_chat_view/video_chat_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'home': ( _ ) => const HomeView(),
+  'login': ( _ ) => const LoginView(),
   'videoChat': ( _ ) => const VideoChatView(),
 };
