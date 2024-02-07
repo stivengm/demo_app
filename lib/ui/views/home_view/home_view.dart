c50bccea8117c07564fb374697e3d90b85c94615
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 19:07:53 -0500

    FIX - Ajuste de configuración JITSI.

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -1,5 +1,8 @@
-import 'package:demo_app/ui/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
+
+import 'package:demo_app/ui/widgets/primary_button.dart';
+
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -31,8 +34,41 @@ class HomeView extends StatelessWidget {
   }
 
   videoChat(BuildContext context) {
+
+    // var options = JitsiMeetConferenceOptions(
+    //   serverURL: "https://meet.jit.si",
+    //   room: "jitsiIsAwesomeWithFlutter",
+    //   configOverrides: {
+    //     "startWithAudioMuted": false,
+    //     "startWithVideoMuted": false,
+    //     "subject" : "Jitsi with Flutter",
+    //   },
+    //   featureFlags: {
+    //     "unsaferoomwarning.enabled": false
+    //   },
+    //   userInfo: JitsiMeetUserInfo(
+    //       displayName: "Flutter user",
+    //       email: "user@example.com"
+    //   ),
+    // );
+
     print("VÍDEO CHAT!");
-    Navigator.pushNamed(context, 'videoChat');
+    var name = "Stiven Gomez";
+    var jitsiMeet = JitsiMeet();
+    var options = JitsiMeetConferenceOptions(
+      serverURL: "https://meet.jit.si/moderated/3c801580063eb8f32274a2c133d0a608c4cbebb6772e7e90b05b366486bd7acc",
+      room: 'jitsiIsAwesome',
+      configOverrides: {
+        "startWithAudioMuted": true,
+        "startWithVideoMuted": true,
+        "subject" : "Conferencia con $name",
+      },
+      userInfo: JitsiMeetUserInfo(
+        displayName: name,
+        email: "stivenalexander7u7@gmail.com"
+      ),
+    );
+    jitsiMeet.join(options);
   }
 
   agendarCita() {
