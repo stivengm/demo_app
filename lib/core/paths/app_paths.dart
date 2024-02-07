bae12419c17b3e6491dbaa5f0a2459f97cca1827
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 20:01:45 -0500

    FIX - Componente chat añadido.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,10 +1,12 @@
-import 'package:demo_app/ui/views/user/login_view/login_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:demo_app/ui/views/home_view/home_view.dart';
+import 'package:demo_app/ui/views/user/login_view/login_view.dart';
+import 'package:demo_app/ui/views/chat_view/chat_view.dart';
 
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'home': ( _ ) => const HomeView(),
-  'login': ( _ ) => const LoginView()
+  'login': ( _ ) => const LoginView(),
+  'chatbot': ( _ ) => const ChatView()
 };
