351395d95294c2a6765f3df406e7efb675afc88a
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 19:58:04 -0500

    FIX - Ajuste de rutas.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -2,11 +2,9 @@ import 'package:demo_app/ui/views/user/login_view/login_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:demo_app/ui/views/home_view/home_view.dart';
-import 'package:demo_app/ui/views/video_chat_view/video_chat_view.dart';
 
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'home': ( _ ) => const HomeView(),
-  'login': ( _ ) => const LoginView(),
-  'videoChat': ( _ ) => const VideoChatView(),
+  'login': ( _ ) => const LoginView()
 };
