8888a99196fb724b9243a9df1c5bb28a4a3d5057
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-06 19:31:36 -0500

    FIX - Ajuste de diseño login.

--- lib/ui/views/user/login_view/login_view.dart
+++ lib/ui/views/user/login_view/login_view.dart
@@ -1,75 +1,114 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+import 'package:demo_app/ui/widgets/primary_button.dart';
+import 'package:demo_app/ui/app_style.dart';
+
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  bool _passwordVisible = true;
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    Size media = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           TextFormField(
-            textInputAction: TextInputAction.next,
-            // controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            autocorrect: false,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            // validator: (email) => email != null && !EmailValidator.validate(email) ? 'Ingrese un email válido' : null,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              labelStyle: const TextStyle(
-                // color: primaryColor
-              ),
-              filled: true,
-              // fillColor: whiteColor,
-              focusedBorder: InputBorder.none,
-              enabledBorder: InputBorder.none,
-              border: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  // color: whiteColor
-                ),
-                borderRadius: BorderRadius.circular(10.0)
-              )
+      body: SizedBox(
+        height: double.infinity,
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                  height: media.height * 1,
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          textInputAction: TextInputAction.next,
+                          controller: emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          // validator: (email) => email != null && !EmailValidator.validate(email) ? 'Ingrese un email válido' : null,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: const TextStyle(
+                              color: primaryColor
+                            ),
+                            filled: true,
+                            fillColor: whiteColor,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: whiteColor
+                              ),
+                              borderRadius: BorderRadius.circular(10.0)
+                            )
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        TextFormField(
+                          obscureText: _passwordVisible,
+                          controller: passwordController,
+                          autocorrect: false,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          // validator: (pass) => pass != null && pass.length < 6 ? 'Ingrese mínimo 6 caracteres' : null,
+                          decoration: InputDecoration(
+                            labelText: 'Contraseña',
+                            labelStyle: const TextStyle(
+                              color: primaryColor
+                            ),
+                            filled: true,
+                            fillColor: whiteColor,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: whiteColor
+                              ),
+                              borderRadius: BorderRadius.circular(10.0)
+                            ),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordVisible
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                            )
+                          )
+                        ),
+                        const SizedBox(height: 30.0),
+                        SizedBox(
+                          width: media.width * .7,
+                          child: PrimaryButton(text: 'Iniciar sesión', onPressed: () => {},),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
-          const SizedBox(height: 15.0),
-          TextFormField(
-            // obscureText: _passwordVisible,
-            // controller: passwordController,
-            autocorrect: false,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (pass) => pass != null && pass.length < 6 ? 'Ingrese mínimo 6 caracteres' : null,
-            decoration: InputDecoration(
-              labelText: 'Contraseña',
-              labelStyle: const TextStyle(
-                // color: primaryColor
-              ),
-              filled: true,
-              // fillColor: whiteColor,
-              focusedBorder: InputBorder.none,
-              enabledBorder: InputBorder.none,
-              border: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  // color: whiteColor
-                ),
-                borderRadius: BorderRadius.circular(10.0)
-              ),
-              // suffixIcon: IconButton(
-              //   icon: Icon(
-              //     _passwordVisible
-              //     ? Icons.visibility_outlined
-              //     : Icons.visibility_off_outlined
-              //   ),
-              //   onPressed: () {
-              //     setState(() {
-              //       _passwordVisible = !_passwordVisible;
-              //     });
-              //   },
-              // )
-            )
-          )
-        ],
+        ),
       ),
     );
   }
